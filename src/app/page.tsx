2c6646e15ced32d49f998fede437f2ed469f7d8d
MarcusQuirino <marcusfilipus@gmail.com>
2024-01-02 11:22:06 -0300

    :3

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+import { CreatePost } from "@/app/_components/create-post";
+import { getServerAuthSession } from "@/server/auth";
+import { api } from "@/trpc/server";
+
+export default async function Home() {
+  return <div>generate new projects idea</div>;
+}
